dde65788c2d85915a808ed529ce1e158ebe8a7a8
pavanHarika <Pavanharika01112025@gmail.com>
2025-10-30 23:00:20 +0530

    my first comit

---
+++ src/main/java/com/groceryapp/grocerymanagement/GrocerymanagementApplication.java
@@ -0,0 +1,13 @@
+package com.groceryapp.grocerymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocerymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrocerymanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groceryapp/grocerymanagement/GrocerymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groceryapp.grocerymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrocerymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
